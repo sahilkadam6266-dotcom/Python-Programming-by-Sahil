08f8cf2f9e52fc975976e25ab54b039964b7aaf9
sahilkadam6266-dotcom <sahilkadam6266@gmail.com>
2026-02-05 11:19:28 +0530

    Experiment 1 Python programming by Sahil

---
+++ Exp1.py
@@ -0,0 +1,33 @@
+name = input("Enter your name: ")
+
+age = input("Enter your age:")
+
+student_class = input("Enter your class:")
+
+division = input("Enter your divisions:")
+
+roll_no = input("Enter your roll number:")
+
+batch = input("Enter your batch:")
+
+phone_no = input("Enter your phone number:")
+
+blood_group = input("Enter your blond group:")
+
+
+print("Student Information")
+
+print("Name", name)
+
+print("Division:", division) 
+
+print("class", student_class)
+
+print("Roll Number:", roll_no)
+
+print( "Batch", batch)
+
+print("Phone Number", phone_no)
+
+print("Blood Group", blood_group)
+
